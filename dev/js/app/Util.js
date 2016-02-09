795395847808fc4fcd245f4443ace151c1b5d8f7
Edwino Stein <edwino.stein@gmail.com>
2016-02-09 15:11:04 -0400

    Corrigindo um problema na estrutura de cores

--- dev/js/app/Util.js
+++ dev/js/app/Util.js
@@ -11,9 +11,17 @@ App.define('Util', {
         green = parseInt(green);
         blue = parseInt(blue);
 
-        this.red = isNaN(red) || red < 0 || red > 255 ? 0 : red;
-        this.green = isNaN(green) || green < 0 || green > 255 ? 0 : green;
-        this.blue = isNaN(blue) || blue < 0 || blue > 255 ? 0 : blue;
+        if(isNaN(red) || red < 0) this.red = 0;
+        else if(red > 255) this.red = 255;
+        else this.red = red;
+
+        if(isNaN(green) || green < 0) this.green = 0;
+        else if(green > 255) this.green = 255;
+        else this.green = green;
+
+        if(isNaN(blue) || blue < 0) this.blue = 0;
+        else if(blue > 255) this.blue = 255;
+        else this.blue = blue;
     },
 
 });
