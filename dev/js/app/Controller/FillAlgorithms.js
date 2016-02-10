042c80a3d8b20fbe9bf4e86f08a301a3ecb9b6d2
Edwino Stein <edwino.stein@gmail.com>
2016-02-10 16:56:08 -0400

    Implementando algoritmo de preenchimento por varredura para polígonos complexos

--- dev/js/app/Controller/FillAlgorithms.js
+++ dev/js/app/Controller/FillAlgorithms.js
@@ -15,9 +15,95 @@ App.define('Controller.FillAlgorithms', {
             case 'circle':
                 this.circleScanLine(polygon.center, polygon.radius, color, borderColor);
             break;
+
+            case 'polygon':
+                this.scanLinePolygon(polygon.vertices, color, borderColor);
+            break;
         }
 
         return this.getTimeStamp() - now;
+    },
+
+    scanLinePolygon: function(vertices, color, borderColor){
+
+        var maxY = vertices[0].y,
+            minY = vertices[0].y;
+
+        for(var i = 1; i < vertices.length; i++){
+            if(maxY < vertices[i].y) maxY = vertices[i].y;
+            if(minY > vertices[i].y) minY = vertices[i].y;
+        }
+
+        var sides = [],
+            sortSides = function(a, b){
+                return (a[0].x + a[1].x)/2 >= (b[0].x + b[1].x)/2 ? 1 : (0);
+            };
+
+
+        for(var i = 0; i < vertices.length; i++){
+            if(i + 1 >= vertices.length)
+                sides.push([vertices[i], vertices[0]]);
+            else
+                sides.push([vertices[i], vertices[i + 1]]);
+        }
+        sides.sort(sortSides);
+
+        var pixelSize = this.getPixelSize(),
+            y = 0,
+            pixelColor = null,
+            intersections = null,
+            pixelInitial = null,
+            pixelFinal = null;
+
+        maxY = this.gridToCanvas(0, this.canvasToGrid(0, maxY).y).y;
+        minY = this.gridToCanvas(0, this.canvasToGrid(0, minY).y + 1).y;
+
+        for(y = minY; y < maxY; y += pixelSize){
+
+            intersections = this.getIntersections(y, sides);
+
+            for(var i in intersections){
+                pixelInitial = this.canvasToGrid(intersections[i][0], y);
+                pixelFinal = this.canvasToGrid(intersections[i][1], y);
+                for(pixelInitial.x; pixelInitial.x <= pixelFinal.x; pixelInitial.x++){
+                    this.grid.activePixel(pixelInitial, color, false);
+                }
+            }
+
+        }
+    },
+
+    getIntersections: function(y, sides){
+
+        var intersections = [],
+            a = [];
+
+        for(var i in sides){
+            if(sides[i][0].y > y && sides[i][1].y < y || sides[i][1].y > y && sides[i][0].y < y){
+                a.push(sides[i]);
+                continue;
+            }
+
+            if(sides[i][0].y === y || sides[i][1].y === y){
+                a.push(sides[i]);
+                continue;
+            }
+        }
+
+        var interval = [];
+        for(var i in a){
+            interval.push(this.getXOfLine(y, a[i][0], a[i][1]));
+            if(interval.length == 2){
+                intersections.push(interval);
+                interval = [];
+            }
+        }
+
+        return intersections;
+    },
+    
+    getXOfLine: function(y, point1, point2){
+        return (((y - point1.y)*(point2.x - point1.x))/(point2.y - point1.y)) + point1.x;
     },
 
     circleScanLine: function(center, radius, color, borderColor){
